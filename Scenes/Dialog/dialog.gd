46c654a97b358b9599eacc0865ddd046049011e5
Vimal Buckley <vimalbuckley@gmail.com>
2026-03-15 09:48:35 -0400

    make dialog auto advance

--- Scenes/Dialog/dialog.gd
+++ Scenes/Dialog/dialog.gd
@@ -1,16 +1,29 @@
 extends Panel
 
-var text: Array[String] = []
+var text: Array[CustomText] = []
+var time_to_close: float = 0
 
 func _ready():
 	visible = false
-	$DialogButton.pressed.connect(close_box)
+	$DialogButton.pressed.connect(advance)
 
+func _process(delta: float) -> void:
+	if (time_to_close > 0):
+		time_to_close -= delta
+		return
+	advance()
 
-func show_box(text):
-	$DialogLabel.text = text
+func set_text(text: Array[CustomText]):
+	self.text = text
+	advance()
+
+func advance():
+	if text.size() == 0:
+		visible = false
+		return
+	var to_show: CustomText = text.pop_front()
+	$DialogLabel.text = to_show.text
+	time_to_close = to_show.timeout
 	visible = true
 	
-func close_box():
-	visible = false
 	
